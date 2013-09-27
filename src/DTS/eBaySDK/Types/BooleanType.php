baac877e1265aee63ace17ca15019b1df928f1f7
David T. Sadler <davidtsadler@googlemail.com>
2013-09-27 22:57:15 +0100

    Make list of class properties a static member.

--- src/DTS/eBaySDK/Types/BooleanType.php
+++ src/DTS/eBaySDK/Types/BooleanType.php
@@ -6,22 +6,22 @@ namespace DTS\eBaySDK\Types;
  */
 class BooleanType extends \DTS\eBaySDK\Types\BaseType
 {
+    private static $propertyTypes = [
+        'value' => [
+            'type' => 'boolean',
+            'unbound' => false,
+            'attribute' => false
+        ]
+    ];
+
     public function __construct(array $values = [])
     {
-        $properties = [
-            'value' => [
-                'type' => 'boolean',
-                'unbound' => false,
-                'attribute' => false
-            ]
-        ];
-
-        list($parentValues, $childValues) = self::getParentValues($properties, $values);
+        list($parentValues, $childValues) = self::getParentValues(self::$propertyTypes, $values);
 
         parent::__construct($parentValues);
 
         if (!array_key_exists(__CLASS__, self::$properties)) {
-            self::$properties[__CLASS__] = array_merge(self::$properties[get_parent_class()], $properties);
+            self::$properties[__CLASS__] = array_merge(self::$properties[get_parent_class()], self::$propertyTypes);
         }
 
         $this->setValues(__CLASS__, $childValues);
